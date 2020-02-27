9aa00d2c3b1a9d0c29a41d55b69e4e23a60b8357
oona baby <oonababy@Noahs-MacBook-Pro-2.local>
2020-02-26 19:19:07 -0500

    jquery solution

--- js/app.js
+++ js/app.js
@@ -1,38 +1,53 @@
-// grab element with id of #roll-dice and listen
-// for click event
-// when click event occurs "call" the rollDice function
-document.getElementById('roll-dice').onclick = rollDice
+$(function() {
+  //
+  // // grab element with id of #roll-dice and listen
+  // // for click event
+  // // when click event occurs "call" the rollDice function
+  console.log("hey this is a message");
+  // document.getElementById('roll-dice').onclick = rollDice
 
-function rollDice () {
-  // randomly generate number from 1 to 6 and store
-  // values in variables (randomDie1 & randomDie2)
-  const randomDie1 = generateRandomDie()
-  const randomDie2 = generateRandomDie()
+// jquery
+  $("#roll-dice").click(rollDice);
 
-  // dynamically generate class names
-  // that correspond to the random numbers
-  const firstDieClass = `dice dice-${randomDie1}`
-  const secondDieClass = `dice dice-${randomDie2}`
+  function rollDice() {
+    // randomly generate number from 1 to 6 and store
+    // values in variables (randomDie1 & randomDie2)
+    const randomDie1 = generateRandomDie();
+    const randomDie2 = generateRandomDie();
 
-  console.log(`randomDie1: ${randomDie1}`)
-  console.log(`randomDie2: ${randomDie2}`)
+    // dynamically generate class names
+    // that correspond to the random numbers
+    const firstDieClass = `dice dice-${randomDie1}`;
+    const secondDieClass = `dice dice-${randomDie2}`;
 
-  console.log(`firstDieClass: ${firstDieClass}`)
-  console.log(`secondDieClass: ${secondDieClass}`)
+    console.log(`randomDie1: ${randomDie1}`);
+    console.log(`randomDie2: ${randomDie2}`);
 
-  updatePage(firstDieClass, secondDieClass)
-}
+    console.log(`firstDieClass: ${firstDieClass}`);
+    console.log(`secondDieClass: ${secondDieClass}`);
 
-// specify function only responsible for
-// generating random numbers
-function generateRandomDie () {
-  return Math.floor(Math.random() * 6) + 1
-}
+    updatePage(firstDieClass, secondDieClass);
+  }
 
-// specify function only responsible for
-// updating our page
-function updatePage (firstClass, secondClass) {
-  // dynamically apply new class names to the dom (html page)
-  document.getElementById('first-die').className = firstClass
-  document.getElementById('second-die').className = secondClass
-}
+  // specify function only responsible for
+  // generating random numbers
+  function generateRandomDie() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  // specify function only responsible for
+  // updating our page
+  function updatePage(firstClass, secondClass) {
+    // dynamically apply new class names to the dom (html page)
+
+  // document.getElementById("first-die").className = firstClass;
+
+// jquery
+$('#first-die').attr("class", firstClass)
+
+//    document.getElementById("second-die").className = secondClass;
+
+//jquery
+$('#second-die').attr("class", secondClass)
+  }
+});
